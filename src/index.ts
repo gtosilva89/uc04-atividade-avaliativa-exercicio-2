d0b8585b2105dafcbbf1eaac6235661e145a916d
Gustavo Silveira Silva <silva.gustavo@hotmail.com.br>
2024-12-11 19:28:40 -0300

    exercicio

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("Olá mundo");
+// Uma fábrica de bolachas produz pacotes com 500 gramas cada e tem disponível um banco de dados contendo o peso de todos os pacotes por lote produzido.
+// A partir desses dados, a fábrica quer saber qual a variação do maior e do menor pacote e quantos pacotes estão acima e quantos pacotes estão abaixo.
+// Caso a variação ocorra em mais de 20% do lote, deve mostrar uma mensagem dizendo "lote com alta divergência", caso contrário, mostrar mensagem, "lote padrão".
+// Gere um array contendo valores aleatórios entre 400 e 600, para poder usá-lo no exercício.
